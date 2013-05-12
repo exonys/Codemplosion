49ee7c79d6e200d2f5cd50de345db3c760821adf
exonys <zadr.true@narod.ru>
2013-05-12 18:17:03 +0600

    HelperLib: Reflection

---
+++ HelperLib/Reflection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace HelperLib
+{
+    public class Reflection
+    {
+        public static Type[] GetTypesInNamespace(Assembly assembly, string nameSpace)
+        {
+            return
+                assembly.GetTypes()
+                        .Where(t => String.Equals(t.Namespace, nameSpace, StringComparison.Ordinal))
+                        .ToArray();
+        }
+    }
+}
